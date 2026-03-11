728c541054d64b6d1de57b78ab2df4ca4e6509fe
Terry Chen <148657482+terryc04@users.noreply.github.com>
2026-03-11 15:43:32 -0700

    fixed py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,61 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
+from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
 async def test_project(dut):
+
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    # start clock
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
-    # Reset
-    dut._log.info("Reset")
+    # reset
+    dut.rst_n.value = 0
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    await Timer(20, units="ns")
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Reset complete")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # ADD 5 + 3 = 8
+    dut.ui_in.value = (3 << 4) | 5
+    dut.uio_in.value = 0
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == 8
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # SUB 10 - 4 = 6
+    dut.ui_in.value = (4 << 4) | 10
+    dut.uio_in.value = 1
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == 6
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # AND
+    dut.ui_in.value = (10 << 4) | 12
+    dut.uio_in.value = 2
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == (12 & 10)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # OR
+    dut.ui_in.value = (1 << 4) | 12
+    dut.uio_in.value = 3
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == (12 | 1)
+
+    # XOR
+    dut.ui_in.value = (3 << 4) | 5
+    dut.uio_in.value = 4
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == (5 ^ 3)
+
+    # SHIFT
+    dut.ui_in.value = (0 << 4) | 5
+    dut.uio_in.value = 5
+    await RisingEdge(dut.clk)
+    assert dut.uo_out.value == (5 << 1)
+
+    dut._log.info("All ALU tests passed")
